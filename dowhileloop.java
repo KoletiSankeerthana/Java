9d3c9b51eb50e46166831d4e847402f255f39134
KoletiSankeerthana <163500274+KoletiSankeerthana@users.noreply.github.com>
2025-09-15 01:26:16 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,10 @@
+public class dowhileloop{
+    public static void main(String[] args) {
+        int i = 1;  // starting value
+
+        do {
+            System.out.println(i);
+            i++;   // increment
+        } while (i <= 5);  // condition checked after executing
+    }
+}
