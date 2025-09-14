86c39d65808f5cf94c6f20c41b0cf9a7addf6c49
Koleti Sankeerthana <sankeerthanakoleti@gmail.com>
2025-09-15 01:16:33 +0530

    Add forloop.java

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String[] args) {
+        // for loop to print numbers 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
