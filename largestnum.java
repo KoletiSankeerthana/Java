79035b5ede129ebfa5524591cc0fff24db601107
Koleti Sankeerthana <sankeerthanakoleti@gmail.com>
2025-09-15 00:37:31 +0530

    Add largestnum.java

---
+++ largestnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class largestnum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest = (a > b && a > c) ? a : (b > c ? b : c);
+
+        System.out.println("Largest number is: " + largest);
+
+        sc.close();
+    }
+}
